e62c32956ad95d002122402fbfc7f8473091aac8
Hugh Cayless <philomousos@gmail.com>
2020-06-02 07:58:00 -0400

    CSS Grid means we can lose horrible, hacky table behavior.

--- src/defaultBehaviors.js
+++ src/defaultBehaviors.js
@@ -74,37 +74,6 @@ export default {
       }],
       ["_", ["(",")"]]
     ],
-    "table": function(elt) {
-      let table = document.createElement("table");
-      table.innerHTML = elt.innerHTML;
-      if (table.firstElementChild.localName == "tei-head") {
-        let head = table.firstElementChild;
-        head.remove();
-        let caption = document.createElement("caption");
-        caption.innerHTML = head.innerHTML;
-        table.appendChild(caption);
-      }
-      for (let row of Array.from(table.querySelectorAll("tei-row"))) {
-        let tr = document.createElement("tr");
-        tr.innerHTML = row.innerHTML;
-        for (let attr of Array.from(row.attributes)) {
-          tr.setAttribute(attr.name, attr.value);
-        }
-        row.parentElement.replaceChild(tr, row);
-      }
-      for (let cell of Array.from(table.querySelectorAll("tei-cell"))) {
-        let td = document.createElement("td");
-        if (cell.hasAttribute("cols")) {
-          td.setAttribute("colspan", cell.getAttribute("cols"));
-        }
-        td.innerHTML = cell.innerHTML;
-        for (let attr of Array.from(cell.attributes)) {
-          td.setAttribute(attr.name, attr.value);
-        }
-        cell.parentElement.replaceChild(td, cell);
-      }
-      return table;
-    },
     "teiHeader": function(e) {
       this.hideContent(e, false);
     },

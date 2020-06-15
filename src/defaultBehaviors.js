86f2d27f480134df2fbf24fcfbd0c408e0119007
Hugh Cayless <philomousos@gmail.com>
2020-06-15 15:32:09 -0400

    Fix for #15.
    Refs and ptrs with multiple @targets will have only the first one
    displayed.

--- src/defaultBehaviors.js
+++ src/defaultBehaviors.js
@@ -8,10 +8,10 @@ export default {
     "eg": ["<pre>","</pre>"],
     // inserts a link inside <ptr> using the @target; the link in the
     // @href is piped through the rw (rewrite) function before insertion
-    "ptr": ["<a href=\"$rw@target\">$@target</a>"],
+    "ptr": ["<a href=\"$rw$first@target\">$@target</a>"],
     // wraps the content of the <ref> in an HTML link
     "ref": [
-      ["[target]", ["<a href=\"$rw@target\">","</a>"]]
+      ["[target]", ["<a href=\"$rw$first@target\">","</a>"]]
     ],
     "graphic": function(elt) {
       let content = new Image();
